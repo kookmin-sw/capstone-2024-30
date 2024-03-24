1728c31ddee1964bb1fa42d01296d08eb2d3b609
jihunchoi <geehoon62017@gmail.com>
2024-03-24 16:32:19 +0900

    feature: 로그인화면에서 메인으로갈 때 못 돌아오게 변경

--- front/capstone_front/lib/screens/login/login_screen.dart
+++ front/capstone_front/lib/screens/login/login_screen.dart
@@ -52,7 +52,7 @@ class _LoginScreenState extends State<LoginScreen> {
                   Flexible(
                     child: InkWell(
                       onTap: () async {
-                        context.push('/');
+                        context.go('/');
                         String result = await _login(
                             '${_userInfo[0]}@kookmin.ac.kr', _userInfo[1]);
                         switch (result) {
