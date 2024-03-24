582131244fd39ab85c48b6bfcc67dbd3f56c07e1
kevinmj12 <kevinmj12@gmail.com>
2024-03-24 20:12:10 +0900

    [FE]#6-회원가입 페이지 구현

--- front/capstone_front/lib/screens/home_screen.dart
+++ front/capstone_front/lib/screens/home_screen.dart
@@ -4,6 +4,7 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'dart:async';
 
 import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
 import 'package:restart_app/restart_app.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -30,8 +31,6 @@ class _HomeScreenState extends State<HomeScreen> {
               icon: const Text("\u{1f1fa}\u{1f1f8}"), // 영어
               onPressed: () async {
                 await storage.write(key: 'language', value: 'english');
-                String? a = await storage.read(key: 'language');
-                print(a);
                 restartDialog(context);
               }),
         ],
