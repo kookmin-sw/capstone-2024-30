ae0e65af7a668b7fb37e0d50c9cf9ebc62457236
khs0316123 <khs0316123@gmail.com>
2024-03-26 03:18:32 +0900

    Hot fix (Network error)
    ConnectionResetError(10054, '현재 연결은 원격 호스트에 의해 강제로 끊겼습니다', None, 10054, None))
    
    -> response에 Agent 정보를 헤더로 넘겨주어 해결

--- ai/crawler/main.py
+++ ai/crawler/main.py
@@ -1,5 +1,8 @@
 import clawler_utils
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 # 현재 스크립트가 위치한 디렉토리 경로를 가져옵니다.
 current_directory = os.path.dirname(os.path.realpath(__file__))
@@ -8,6 +11,6 @@ os.chdir(current_directory)
 
 
 # 공지사항 크롤링 시작
-clawler_utils.Clawling_notice_page_to_page(start=1, end=2, path='./data/Notice/') #1~194
+clawler_utils.Clawling_notice_page_to_page(start=1, end=194, path='./data/Notice/') #1~194
 
 # 16, 7
