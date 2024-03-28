efb216b2fef75ff726dd5a53223466aa2b2f326a
khs0316123 <khs0316123@gmail.com>
2024-03-28 17:20:27 +0900

    bug fix
    run_chatbot.py 실행시 경로 변경 및 FASSIS 파일 수정

--- ai/crawler/clova_ocr.py
+++ ai/crawler/clova_ocr.py
@@ -2,8 +2,11 @@ import requests
 import json
 import time
 import uuid
+<<<<<<< Updated upstream
 import os
 
+=======
+>>>>>>> Stashed changes
 
 class OCR:
   def __init__(self, secret_key, api_url):
--- ai/main.py
+++ ai/main.py
@@ -3,6 +3,10 @@ from vectordb.vector_db import VectorDB
 from dotenv import load_dotenv
 import os
 
+# 현재 스크립트가 위치한 디렉토리 경로를 가져옵니다.
+current_directory = os.path.dirname(os.path.realpath(__file__))
+os.chdir(current_directory)
+
 load_dotenv()
 
 api_key = os.getenv('OPENAI_API_KEY')
