ae8b965b5e64531b4bd78bbc3988d0f357eba0a1
BlueBerrySoda <opace2341@kookmin.ac.kr>
2024-04-05 01:22:20 +0900

    feat: #11-API 호출을 위한 메서드 수정

--- back/src/main/java/com/example/capstone/domain/menu/controller/MenuController.java
+++ back/src/main/java/com/example/capstone/domain/menu/controller/MenuController.java
@@ -15,15 +15,9 @@ import java.time.LocalDateTime;
 public class MenuController {
     private final MenuService menuService;
 
-    @GetMapping("/api/menu/test")
-    public void getMenuTest(){
-        LocalDateTime test = LocalDateTime.of(2024, 3, 26, 0, 0);
-        menuService.addMenus(test);
-    }
-
+    @ResponseBody
     @GetMapping("/api/menu/daily")
-    public String getMenuByDate(){//@RequestBody LocalDateTime date, @RequestBody String language){
-        LocalDateTime test = LocalDateTime.of(2024, 3, 26, 0, 0);
-        return menuService.findMenuByDate(test, "EN-US").;
+    public String getMenuByDate(@RequestBody LocalDateTime date, @RequestBody String language){
+        return menuService.findMenuByDate(date, language);
     }
 }
