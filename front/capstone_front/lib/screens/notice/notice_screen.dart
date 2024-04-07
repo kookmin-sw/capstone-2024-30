b3da899437be0a75c4b5b7a682553e06b40b08bb
jihunchoi <geehoon62017@gmail.com>
2024-04-07 17:14:53 +0900

    feat: #54-테스트 데이터 파일 분리

--- front/capstone_front/lib/screens/notice/notice_screen.dart
+++ front/capstone_front/lib/screens/notice/notice_screen.dart
@@ -1,7 +1,6 @@
+import 'package:capstone_front/screens/notice/test_notice_data.dart';
 import 'package:capstone_front/screens/notice_detail/notice_detail_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:go_router/go_router.dart';
 
 class NoticeScreen extends StatefulWidget {
   const NoticeScreen({super.key});
@@ -14,65 +13,6 @@ class _NoticeScreenState extends State<NoticeScreen> {
   List<String> items = ['전체공지', '학사공지', '장학공지'];
   String selectedItem = '전체공지';
   final _controller = TextEditingController();
-
-  final List<Map<String, dynamic>> posts = [
-    {
-      'title': '미래혁신단(차세대통신사업단) 직원 모집',
-      'kind': '교내채용',
-      'date': '2024-04-05',
-    },
-    {
-      'title': '국민대학교 비전임교원 채용(2024.05.01.자)',
-      'kind': '교내채용',
-      'date': '2024-04-05',
-    },
-    {
-      'title': '(진로지원센터) 계약직원 모집',
-      'kind': '교내채용',
-      'date': '2024-04-04',
-    },
-    {
-      'title': '(대외협력팀) 계약직원 모집',
-      'kind': '교내채용',
-      'date': '2024-04-04',
-    },
-    {
-      'title': '[장학공지] 2024학년도 1학기 남윤철장학금 신청 안내',
-      'kind': '장학공지',
-      'date': '2024-04-04',
-    },
-    {
-      'title': '[장학공지] 2024학년도 1학기 가온누리장학금 신청 안내',
-      'kind': '장학공지',
-      'date': '2024-04-04',
-    },
-    {
-      'title': '[장학공지] 2024학년도 1학기 어울림장학금 신청 안내',
-      'kind': '장학공지',
-      'date': '2024-04-04',
-    },
-    {
-      'title': '[장학공지] 2024학년도 1학기 그린나래장학금 신청 안내',
-      'kind': '장학공지',
-      'date': '2024-04-04',
-    },
-    {
-      'title': '[장학공지] 2024학년도 1학기 메시장학금 신청 안내',
-      'kind': '장학공지',
-      'date': '2024-04-03',
-    },
-  ];
-
-  final List<String> noticeKinds = [
-    '전체공지',
-    '학사공지',
-    '장학공지',
-    '교내채용',
-    '대학입학공지',
-    '대학원입학공지',
-    '행정공지',
-    '특강공지'
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -144,28 +84,30 @@ class _NoticeScreenState extends State<NoticeScreen> {
                               topRight: Radius.circular(20.0),
                             ),
                           ),
-                          child: Wrap(
-                            children: <Widget>[
-                              ...noticeKinds.map(
-                                (item) => ListTile(
-                                  onTap: () {
-                                    setState(() {
-                                      selectedItem = item;
-                                    });
-                                    Navigator.of(context).pop();
-                                  },
-                                  title: Center(
-                                    child: Text(
-                                      item,
-                                      style: const TextStyle(fontSize: 20),
+                          child: SingleChildScrollView(
+                            child: Wrap(
+                              children: <Widget>[
+                                ...noticeKinds.map(
+                                  (item) => ListTile(
+                                    onTap: () {
+                                      setState(() {
+                                        selectedItem = item;
+                                      });
+                                      Navigator.of(context).pop();
+                                    },
+                                    title: Center(
+                                      child: Text(
+                                        item,
+                                        style: const TextStyle(fontSize: 20),
+                                      ),
                                     ),
                                   ),
                                 ),
-                              ),
-                              const SizedBox(
-                                height: 20,
-                              ),
-                            ],
+                                const SizedBox(
+                                  height: 20,
+                                ),
+                              ],
+                            ),
                           ),
                         );
                       },
