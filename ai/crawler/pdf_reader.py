1d8e879ebbbf05b3a982e7af0e0d27b44766a1f2
khs0316123 <khs0316123@gmail.com>
2024-04-13 19:31:18 +0900

    pdf reader에 tqdm 추가

--- ai/crawler/pdf_reader.py
+++ ai/crawler/pdf_reader.py
@@ -1,6 +1,7 @@
 from langchain_community.document_loaders import PyPDFLoader
 import pickle
 import os
+from tqdm import tqdm
 
 class PdfReader:
     def __init__(self) -> None:
@@ -13,10 +14,11 @@ class PdfReader:
 
         if not os.path.exists(path):
             os.makedirs(path)
-
+        print(f'-- Load pdf file {filename} --')
         loader = PyPDFLoader(filepath)
         pages = loader.load()
-        for page_no in range(len(pages)):
+        print('-- start --')
+        for page_no in tqdm(range(len(pages))):
             doc = pages[page_no]
             doc.page_content = doc.page_content.replace(u"\xa0", u" ")
             if doc.page_content:
