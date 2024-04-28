1b7a7db65c99dd1e35386df361ca3bd33f097aac
BlueBerrySoda <opace2341@kookmin.ac.kr>
2024-04-29 05:30:30 +0900

    feat: #84-댓글 생성 Controller Endpoint 작성

--- back/src/main/java/com/example/capstone/domain/qna/controller/AnswerController.java
+++ back/src/main/java/com/example/capstone/domain/qna/controller/AnswerController.java
@@ -1,16 +1,31 @@
 package com.example.capstone.domain.qna.controller;
 
+import com.example.capstone.domain.jwt.JwtTokenProvider;
+import com.example.capstone.domain.qna.dto.AnswerListRequest;
+import com.example.capstone.domain.qna.dto.AnswerPostRequest;
+import com.example.capstone.domain.qna.dto.AnswerPutRequest;
+import com.example.capstone.domain.qna.dto.QuestionPostRequest;
+import com.example.capstone.domain.qna.service.AnswerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/api/answer")
 public class AnswerController {
-    //TODO 댓글 작성하기
-    //TODO 댓글 불러오기
-    //TODO 댓글 수정하기
-    //TODO 댓글 삭제하기
+    private final AnswerService answerService;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createAnswer(/*@RequestHeader String token,*/ @RequestBody AnswerPostRequest request) {
+        String userId = UUID.randomUUID().toString(); //jwtTokenProvider.extractUUID(token);
+        return ResponseEntity.ok(answerService.createAnswer(userId, request));
+    }
+
 
 }
