f4a3e730f4cc1f17b751d72e1e0670485f94cad1
BlueBerrySoda <opace2341@kookmin.ac.kr>
2024-04-29 05:48:46 +0900

    feat: (#84, #47)-FAQ글 생성 기능 Controller Endpoint 작성

--- back/src/main/java/com/example/capstone/domain/qna/controller/FAQController.java
+++ back/src/main/java/com/example/capstone/domain/qna/controller/FAQController.java
@@ -1,18 +1,45 @@
 package com.example.capstone.domain.qna.controller;
 
+import com.example.capstone.domain.jwt.JwtTokenProvider;
+import com.example.capstone.domain.qna.dto.FAQListRequest;
+import com.example.capstone.domain.qna.dto.FAQPostRequest;
+import com.example.capstone.domain.qna.dto.FAQPutRequest;
+import com.example.capstone.domain.qna.dto.FAQResponse;
+import com.example.capstone.domain.qna.entity.FAQ;
+import com.example.capstone.domain.qna.service.FAQService;
+import com.example.capstone.domain.qna.service.ImageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static org.springframework.http.HttpStatus.CREATED;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/api/faq")
 public class FAQController {
-    //TODO faq 작성하기
-    //TODO faq 불러오기
-    //TODO faq 목록 불러오기
-    //TODO faq 수정하기
-    //TODO faq 삭제하기
-    //TODO faq 검색하기
+    private final FAQService faqService;
+
+    private final ImageService imageService;
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createFAQ(@RequestPart FAQPostRequest request,
+                                       @RequestPart List<MultipartFile> multipartFileList) {
+        List<String> urlList = new ArrayList<>();
+        FAQResponse faq = faqService.createFAQ(request);
+        if(multipartFileList != null) {
+            urlList = imageService.upload(multipartFileList, faq.id(), true);
+        }
+        return ResponseEntity.ok(Map.of("content", faq, "imgUrl", urlList));
+    }
+
 
 }
