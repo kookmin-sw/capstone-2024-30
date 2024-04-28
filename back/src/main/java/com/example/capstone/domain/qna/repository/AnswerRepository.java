b14651f5fa8c019bb2f7880f1513dcede2446d82
BlueBerrySoda <opace2341@kookmin.ac.kr>
2024-04-29 06:23:45 +0900

    feat: #84-댓글 Repository 구현

--- back/src/main/java/com/example/capstone/domain/qna/repository/AnswerRepository.java
+++ back/src/main/java/com/example/capstone/domain/qna/repository/AnswerRepository.java
@@ -1,4 +1,16 @@
 package com.example.capstone.domain.qna.repository;
 
-public interface AnswerRepository {
+import com.example.capstone.domain.qna.entity.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AnswerRepository extends JpaRepository<Answer, Long>, AnswerListRepository {
+    Answer save(Answer answer);
+
+    Optional<Answer> findById(Long id);
+
+    void deleteById(Long id);
 }
