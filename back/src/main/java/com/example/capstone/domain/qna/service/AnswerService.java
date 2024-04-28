9413009b4d6b171b1a01ee29df5922ea623c2b95
BlueBerrySoda <opace2341@kookmin.ac.kr>
2024-04-29 06:07:04 +0900

    feat: #84-댓글 생성 Service 비즈니스 로직 구현

--- back/src/main/java/com/example/capstone/domain/qna/service/AnswerService.java
+++ back/src/main/java/com/example/capstone/domain/qna/service/AnswerService.java
@@ -1,4 +1,40 @@
 package com.example.capstone.domain.qna.service;
 
+import com.example.capstone.domain.qna.dto.AnswerListResponse;
+import com.example.capstone.domain.qna.dto.AnswerPostRequest;
+import com.example.capstone.domain.qna.dto.AnswerPutRequest;
+import com.example.capstone.domain.qna.dto.AnswerResponse;
+import com.example.capstone.domain.qna.entity.Answer;
+import com.example.capstone.domain.qna.entity.Question;
+import com.example.capstone.domain.qna.repository.AnswerRepository;
+import com.example.capstone.domain.qna.repository.QuestionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
 public class AnswerService {
+    private final AnswerRepository answerRepository;
+    private final QuestionRepository questionRepository;
+
+    public AnswerResponse createAnswer(String userId, AnswerPostRequest request) {
+        LocalDateTime current = LocalDateTime.now();
+        Question questionId = questionRepository.findById(request.questionId()).get();
+        Answer answer = answerRepository.save(Answer.builder().question(questionId).author(request.author())
+                .context(request.context()).createdDate(current).updatedDate(current).likeCount(0L).uuid(UUID.fromString(userId)).build());
+        return answer.toDTO();
+    }
+
+
+
+    //TODO 응답 통일
 }
