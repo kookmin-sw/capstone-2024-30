1bd352b12d982c947b8ac71eccae9d4d22eed73d
BlueBerrySoda <opace2341@kookmin.ac.kr>
2024-04-29 06:20:32 +0900

    feat: #84-질문글 생성 기능 Service 비즈니스 로직 구현

--- back/src/main/java/com/example/capstone/domain/qna/service/QuestionService.java
+++ back/src/main/java/com/example/capstone/domain/qna/service/QuestionService.java
@@ -1,40 +1,36 @@
 package com.example.capstone.domain.qna.service;
 
-import com.example.capstone.domain.qna.dto.QuestionPostRequest;
+import com.example.capstone.domain.qna.dto.*;
 import com.example.capstone.domain.qna.entity.Question;
+import com.example.capstone.domain.qna.repository.AnswerRepository;
 import com.example.capstone.domain.qna.repository.QuestionRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class QuestionService {
-    public final QuestionRepository questionRepository;
+    private final QuestionRepository questionRepository;
+    private final AnswerRepository answerRepository;
 
-    public void createQuestion(String userId, QuestionPostRequest request) {
+    public QuestionResponse createQuestion(String userId, QuestionPostRequest request) {
         LocalDateTime current = LocalDateTime.now();
-        questionRepository.save(Question.builder().title(request.title()).context(request.context())
-                .author(request.author()).createdDate(current).updatedDate(current)
-                .uuid(UUID.fromString(userId)).build());
+        Question quest = questionRepository.save(Question.builder().title(request.title()).context(request.context())
+                .author(request.author()).createdDate(current).updatedDate(current).tag(request.tag())
+                .country(request.country()).uuid(UUID.fromString(userId)).build());
+
+        return quest.toDto();
     }
 
-    public Question readQuestion(Long id) {
-        Question quest = questionRepository.findById(id).get();
-        return quest;
-    }
 
-    @Transactional
-    public void updateQuestion(String userId, Long id, QuestionPostRequest request) {
-        LocalDateTime current = LocalDateTime.now();
-        Question quest = questionRepository.findById(id).get();
-        quest.update(request.title(), request.context(), current);
-    }
 
-    public void eraseQuestion(Long id){
-        questionRepository.deleteById(id);
-    }
+    //TODO 응답 통일
 }
