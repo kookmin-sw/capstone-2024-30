76e6111991997406a8ea699ee43a6bf22ea7ea5a
BlueBerrySoda <opace2341@kookmin.ac.kr>
2024-04-29 06:25:38 +0900

    feat: #84-질문글 Repository 구현

--- back/src/main/java/com/example/capstone/domain/qna/repository/QuestionRepository.java
+++ back/src/main/java/com/example/capstone/domain/qna/repository/QuestionRepository.java
@@ -1,13 +1,16 @@
 package com.example.capstone.domain.qna.repository;
 
 import com.example.capstone.domain.qna.entity.Question;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface QuestionRepository extends JpaRepository<Question, Long>, QuestionCursorRepository {
+public interface QuestionRepository extends JpaRepository<Question, Long>, QuestionListRepository {
 
     Question save(Question quest);
 
