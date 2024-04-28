7b3eda1649e9e49cbfa9e026b2978d3b6b78f79b
BlueBerrySoda <opace2341@kookmin.ac.kr>
2024-04-29 06:00:58 +0900

    feat: #84-댓글 Entity 수정

--- back/src/main/java/com/example/capstone/domain/qna/entity/Answer.java
+++ back/src/main/java/com/example/capstone/domain/qna/entity/Answer.java
@@ -1,9 +1,12 @@
 package com.example.capstone.domain.qna.entity;
 
+import com.example.capstone.domain.qna.dto.AnswerResponse;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -18,15 +21,18 @@ public class Answer {
     @Column(name = "id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "question_id")
-    private Question questionId;
+    private Question question;
 
     @Column(name = "author", nullable = false)
     private String author;
 
     @Column(name = "context", columnDefinition = "LONGTEXT", nullable = false)
     private String context;
+
+    @Column(name = "like_count", nullable = false)
+    private Long likeCount;
 
     @Column(name = "created_date", nullable = false)
     private LocalDateTime createdDate;
@@ -36,4 +42,6 @@ public class Answer {
 
     @Column(name = "uuid", nullable = false, unique = true)
     private UUID uuid;
+
+
 }
