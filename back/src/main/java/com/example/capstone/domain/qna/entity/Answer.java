5b06ae0fda1bb49be6e5649e7538faeada8793d2
BlueBerrySoda <opace2341@kookmin.ac.kr>
2024-04-29 06:01:27 +0900

    feat: #84-댓글 Entity 메서드 구현

--- back/src/main/java/com/example/capstone/domain/qna/entity/Answer.java
+++ back/src/main/java/com/example/capstone/domain/qna/entity/Answer.java
@@ -43,5 +43,20 @@ public class Answer {
     @Column(name = "uuid", nullable = false, unique = true)
     private UUID uuid;
 
+    public void update(String context, LocalDateTime updatedDate) {
+        this.context = context;
+        this.updatedDate = updatedDate;
+    }
 
+    public void upLikeCount() {
+        this.likeCount += 1;
+    }
+
+    public void downLikeCount() {
+        this.likeCount -= 1;
+    }
+
+    public AnswerResponse toDTO() {
+        return new AnswerResponse(id, question, author, context, likeCount, createdDate, updatedDate, uuid);
+    }
 }
