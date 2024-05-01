b167c678d5c956ba4ebc1fe18ff03366049033a5
mclub4 <mclub4@kookmin.ac.kr>
2024-05-02 01:53:39 +0900

    feat: #115-CICD 결과 자동으로 Slack 전송

--- back/src/main/java/com/example/capstone/domain/speech/service/SpeechService.java
+++ back/src/main/java/com/example/capstone/domain/speech/service/SpeechService.java
@@ -37,8 +37,6 @@ public class SpeechService {
     @Async
     public CompletableFuture<Map<String, Object>> pronunciation(String compareText, MultipartFile file) throws InterruptedException, ExecutionException, IOException {
         SpeechConfig speechConfig = SpeechConfig.fromSubscription(speechKey, speechRegion);
-        speechConfig.setProperty(PropertyId.Speech_LogFilename, "/container/path/to/logs/speech_sdk.log");
-        System.out.println(file);
 
         WavStream wavStream = new WavStream(file.getInputStream());
         PullAudioInputStream inputStream = PullAudioInputStream.createPullStream(wavStream, wavStream.getFormat());
