00f903884a901c044a8b8e5daadc9f3f6dfe7fee
BlueBerrySoda <opace2341@kookmin.ac.kr>
2024-05-03 04:58:54 +0900

    feat: #119-헬퍼 매칭글 생성 Service 구현

---
+++ back/src/main/java/com/example/capstone/domain/help/service/HelpService.java
@@ -0,0 +1,33 @@
+package com.example.capstone.domain.help.service;
+
+import com.example.capstone.domain.help.dto.*;
+import com.example.capstone.domain.help.repository.HelpListRepository;
+import com.example.capstone.domain.help.repository.HelpRepository;
+import com.example.capstone.domain.help.entity.Help;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class HelpService {
+    private final HelpRepository helpRepository;
+
+    public HelpResponse createHelp(String userId, HelpPostRequest request) {
+        LocalDateTime current = LocalDateTime.now();
+        Help help = helpRepository.save(Help.builder().title(request.title()).context(request.context())
+                .author(request.author()).createdDate(current).updatedDate(current).isHelper(request.isHelper())
+                .isDone(false).country(request.country()).uuid(UUID.fromString(userId)).build());
+
+        return help.toDTO();
+    }
+
+
+}
