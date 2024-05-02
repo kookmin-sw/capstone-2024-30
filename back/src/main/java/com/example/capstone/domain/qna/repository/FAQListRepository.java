efe24e337e736758f68d48fbb6a5ae2ac08dfa67
BlueBerrySoda <opace2341@kookmin.ac.kr>
2024-05-03 04:45:20 +0900

    refactor: #118-Swagger 기능을 위한 리팩토링

--- back/src/main/java/com/example/capstone/domain/qna/repository/FAQListRepository.java
+++ back/src/main/java/com/example/capstone/domain/qna/repository/FAQListRepository.java
@@ -1,11 +1,12 @@
 package com.example.capstone.domain.qna.repository;
 
 import com.example.capstone.domain.qna.dto.FAQListResponse;
+import com.example.capstone.domain.qna.dto.FAQSliceResponse;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 
 import java.util.Map;
 
 public interface FAQListRepository {
-    Map<String, Object> getFAQListByPaging(Long cursorId, Pageable page, String language, String word, String tag);
+    FAQSliceResponse getFAQListByPaging(Long cursorId, Pageable page, String language, String word, String tag);
 }
