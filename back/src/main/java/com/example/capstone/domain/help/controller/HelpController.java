b42717e3c245da9e23829901e941499585aab143
BlueBerrySoda <opace2341@kookmin.ac.kr>
2024-05-02 17:33:17 +0900

    헬퍼매칭 Controller 생성

---
+++ back/src/main/java/com/example/capstone/domain/help/controller/HelpController.java
@@ -0,0 +1,13 @@
+package com.example.capstone.domain.help.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/api/help")
+public class HelpController {
+
+
+}
