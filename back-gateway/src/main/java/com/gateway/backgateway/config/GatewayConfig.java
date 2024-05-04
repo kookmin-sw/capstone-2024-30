d4985f9fef18a5e3833f3dad8cafc54e29b0e378
mclub4 <mclub4@kookmin.ac.kr>
2024-05-04 16:58:08 +0900

    fix: Swagger 경로 오류 수정

--- back-gateway/src/main/java/com/gateway/backgateway/config/GatewayConfig.java
+++ back-gateway/src/main/java/com/gateway/backgateway/config/GatewayConfig.java
@@ -25,7 +25,7 @@ public class GatewayConfig {
                 .route("business", r -> r.path("/api/user/signin", "/api/user/test", "/api/user/signup",
                                 "/api/announcement/**", "/api/menu/**", "/api/speech"
                                 , "/api/auth/**", "/api/swagger-ui/**", "/api/api-docs/**")
-                        .uri("http://localhost:8080"))
+                        .uri("http://spring:8080"))
                 .route("business", r -> r.path("/api/**")
                         .filters(f->f.filter(authFilter.apply(config -> {config.setRequiredRole("role_user");})))
                         .uri("http://spring:8080"))
