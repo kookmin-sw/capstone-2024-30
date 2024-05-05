78ab0b793166c3f5c53fdfee9021a6a50a332ce9
BlueBerrySoda <opace2341@kookmin.ac.kr>
2024-05-06 00:40:49 +0900

    feat: #136-추천 관리 생성 Service 구현

--- back/src/main/java/com/example/capstone/domain/star/service/StarService.java
+++ back/src/main/java/com/example/capstone/domain/star/service/StarService.java
@@ -1,10 +1,13 @@
 package com.example.capstone.domain.star.service;
 
+import com.example.capstone.domain.star.repository.StarRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class StarService {
+    private final StarRepository starRepository;
 
+    public void createStar()
 }
