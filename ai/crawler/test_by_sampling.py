d54364ff18cd47b571a433b4cbb68275027a1f9f
khs0316123 <khs0316123@gmail.com>
2024-05-11 17:47:16 +0900

    first commit

--- ai/crawler/test_by_sampling.py
+++ ai/crawler/test_by_sampling.py
@@ -10,7 +10,7 @@ parent_dir = os.path.dirname(current_dir)
 os.chdir(parent_dir)
 
 # 파일에서 객체를 불러옵니다.
-with open('./data/SW/sw_notice.pkl', 'rb') as f:
+with open('./data/NAVER/kookhee_kmu_0_3.pkl', 'rb') as f:
     loaded_data1 = pickle.load(f, encoding='utf-8')
 
 # 불러온 객체를 출력합니다.
@@ -18,9 +18,9 @@ with open('./data/SW/sw_notice.pkl', 'rb') as f:
 # print(type(loaded_data2[0]))
 #print(loaded_data1)
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=50)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=50)
 
-splits1 = text_splitter.split_documents(loaded_data1)[5]
+splits1 = text_splitter.split_documents(loaded_data1)
 print(splits1)
 print('++++++++++++++++++++++++++++')
 #print(splits2)
--- ai/llm/llm_rag.py
+++ ai/llm/llm_rag.py
@@ -3,8 +3,9 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough, RunnableLambda
 from langchain_openai import ChatOpenAI
 from tavily import TavilyClient
-from llm.prompt import casual_prompt, is_qna_prompt, combine_result_prompt, score_prompt, contextualize_prompt
+from llm.prompt import casual_prompt, is_qna_prompt, score_prompt, contextualize_prompt, rag_prompt
 from langchain.retrievers.multi_query import MultiQueryRetriever
+from langchain.retrievers import EnsembleRetriever
 from langchain.memory import ChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
@@ -17,10 +18,9 @@ class LLM_RAG:
         self.question = ''
         self.llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
         self.tavily = TavilyClient(os.getenv('TAVILY_API_KEY'))
-        self.rag_prompt = hub.pull("rlm/rag-prompt")
+        self.rag_prompt = rag_prompt()
         self.casual_prompt = casual_prompt()
         self.is_qna_prompt = is_qna_prompt()
-        self.combine_result_prompt = combine_result_prompt()
         self.score_prompt = score_prompt()
         self.contextualize_prompt = contextualize_prompt()
         self.deepl = deepl.Translator(os.getenv("DEEPL_API_KEY"))
@@ -28,8 +28,11 @@ class LLM_RAG:
         self.result_lang = None
         self.notice_retriever = None
         self.school_retriever = None
+        self.naver_retriever = None
         self.notice_multiquery_retriever = None
         self.school_multiquery_retriever = None
+        self.naver_multiquery_retriever = None
+        self.ensemble_retriever = None
         self.store = {}
         self.session_id = None
         self.ephemeral_chat_history = ChatMessageHistory()
@@ -52,10 +55,20 @@ class LLM_RAG:
         elif data_type == 'school_info':
             self.school_retriever = retriever
             self.school_multiquery_retriever = MultiQueryRetriever.from_llm(retriever=retriever, llm=self.llm)
+
+        elif data_type == 'naver':
+            self.naver_retriever = retriever
+            self.naver_multiquery_retriever = MultiQueryRetriever.from_llm(retriever=retriever, llm=self.llm)
+
         else:
             print('Choose valid type!')
 
     def set_chain(self):
+
+        self.ensemble_retriever = EnsembleRetriever(
+            retrievers= [self.notice_retriever, self.school_retriever, self.naver_retriever],
+            weights=[0.2, 0.3, 0.5])
+
         self.qna_router = (
             self.is_qna_prompt
             | self.llm
@@ -73,23 +86,9 @@ class LLM_RAG:
             | StrOutputParser()
         )
 
-        self.notice_rag_chain = (
-            {"context": self.notice_multiquery_retriever | self.format_docs,  "question": RunnablePassthrough()}
+        self.rag_chain = (
+            {"context": self.ensemble_retriever | self.format_docs,  "question": RunnablePassthrough()}
             | self.rag_prompt
-            | self.llm
-            | StrOutputParser()
-        )
-        
-        self.schl_info_rag_chain = (
-            {"context":  self.school_multiquery_retriever | self.format_docs, "question": RunnablePassthrough()}
-            | self.rag_prompt
-            | self.llm
-            | StrOutputParser()
-        )
-
-        self.rag_combine_chain = (
-            {"notice_info": self.notice_rag_chain, "school_info": self.schl_info_rag_chain, "question": RunnablePassthrough()}
-            | self.combine_result_prompt
             | self.llm
             | StrOutputParser()
         )
@@ -125,7 +124,7 @@ class LLM_RAG:
             self.store[self.session_id] = ChatMessageHistory()
     
         if "question" in info["topic"].lower():
-            self.result = self.rag_combine_chain.invoke(self.ko_query)
+            self.result = self.rag_chain.invoke(self.ko_query)
             score = self.score_chain.invoke({"question" : self.ko_query, "answer": self.result})
             self.score_invoke_chain.invoke({"score" : score, "question": self.ko_query})
         
@@ -133,7 +132,7 @@ class LLM_RAG:
             self.result =  self.casual_answer_chain.invoke(self.ko_query)
 
         else:
-            self.result = self.rag_combine_chain.invoke(self.ko_query)
+            self.result = self.rag_chain.invoke(self.ko_query)
 
         
     def score_route(self, info):
@@ -147,9 +146,13 @@ class LLM_RAG:
             base = self.deepl.translate_text(base, target_lang=self.result_lang).text
             self.result = base + content
 
+    # def format_docs(self, docs):
+    # # 검색한 문서 결과를 하나의 문단으로 합쳐줍니다.
+    #     return "\n\n".join(doc.page_content + '\nmetadata=' + str(doc.metadata) for doc in docs)
+
     def format_docs(self, docs):
     # 검색한 문서 결과를 하나의 문단으로 합쳐줍니다.
-        return "\n\n".join(doc.page_content + '\nmetadata=' + str(doc.metadata) for doc in docs)
+        return "\n\n".join(doc.page_content for doc in docs)
     
     def query(self, question, result_lang, session_id='unused'):
         self.question = question
