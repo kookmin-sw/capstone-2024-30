db24dc34885c8a60041b029c7d28e5ed08ce89ee
kevinmj12 <kevinmj12@gmail.com>
2024-05-14 16:31:56 +0900

    refactor:#198/FAQ 그림자 수정, 검색창 수정

--- front/capstone_front/lib/main.dart
+++ front/capstone_front/lib/main.dart
@@ -29,6 +29,7 @@ import 'package:capstone_front/screens/speech_practice/speech_practice_screen.da
 import 'package:capstone_front/screens/speech_practice/speech_screen.dart';
 import 'package:capstone_front/services/cafeteria_menu_service.dart';
 import 'package:capstone_front/utils/page_animation.dart';
+import 'package:capstone_front/utils/search_screen.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -97,17 +98,17 @@ Future<void> getMenus() async {
 
 Future<void> getUserInfo() async {
   const storage = FlutterSecureStorage();
-  String? _userName = (await storage.read(key: "userName"));
-  if (_userName == null) {
+  String? userName = (await storage.read(key: "userName"));
+  if (userName == null) {
     userName = '';
   } else {
-    userName = _userName;
+    userName = userName;
   }
-  String? _userMajor = (await storage.read(key: "userMajor"));
-  if (_userMajor == null) {
+  String? userMajor = (await storage.read(key: "userMajor"));
+  if (userMajor == null) {
     userMajor = '';
   } else {
-    userMajor = _userMajor;
+    userMajor = userMajor;
   }
 }
 
--- front/capstone_front/lib/screens/question/question_screen.dart
+++ front/capstone_front/lib/screens/question/question_screen.dart
@@ -1,6 +1,9 @@
 import 'package:capstone_front/screens/faq/faq_screen.dart';
 import 'package:capstone_front/screens/qna/qna_list_screen/qna_list_screen.dart';
+import 'package:capstone_front/utils/search_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:capstone_front/screens/qna/qna_list_screen/qna_list_screen.dart';
+import 'package:go_router/go_router.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -12,9 +15,11 @@ class QuestionScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionScreen> {
   int _selectedPageIndex = 0;
   final _qnaScreenList = [
-    const QnaListScreen(),
+    QnaListScreen(key: qnaListScreenGlobalKey),
     const FaqScreen(),
   ];
+
+  String qnaSearchWord = 'what';
 
   @override
   Widget build(BuildContext context) {
@@ -53,6 +58,31 @@ class _QuestionScreenState extends State<QuestionScreen> {
             ),
           ],
         ),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.search,
+              color: Theme.of(context).primaryColor,
+            ),
+            onPressed: () {
+              // QNA 검색
+              if (_selectedPageIndex == 0) {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => SearchScreen(
+                            searchCallback: (text) {
+                              qnaListScreenGlobalKey.currentState
+                                  ?.searchQna(text);
+                            },
+                          )),
+                );
+              }
+              // FAQ 검색
+              else {}
+            },
+          ),
+        ],
       ),
       body: SafeArea(
         child: _qnaScreenList.elementAt(_selectedPageIndex),
