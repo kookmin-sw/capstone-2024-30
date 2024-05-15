bd2bd6d457a11ef3f230253c61fbc9fe94cffe82
guahama <chldudfkr119@kookmin.ac.kr>
2024-05-15 12:28:55 +0900

    api update

--- ai/server.py
+++ ai/server.py
@@ -1,5 +1,6 @@
 from typing import Union, Optional
-from fastapi import FastAPI, UploadFile, BackgroundTasks, Header
+from fastapi import FastAPI, UploadFile, BackgroundTasks, Request
+from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from pydantic import BaseModel
@@ -9,13 +10,13 @@ from dotenv import load_dotenv
 import os
 import uvicorn
 import datetime
-import schedule
-import time
 from database import Engineconn
-from models import Announcement, AnnouncementFile
+from models import Announcement
 import sched
 from apscheduler.schedulers.background import BackgroundScheduler
 import logging
+from langchain_core.documents.base import Document
+import pickle
  
 sched = BackgroundScheduler(timezone='Asia/Seoul')
 
@@ -23,11 +24,14 @@ class Query(BaseModel):
     query: str
     target_lang: str
 
+class CustomException(Exception):
+    def __init__(self, name: str):
+        self.name = name
+
 @asynccontextmanager
 async def lifespan(app:FastAPI):
     global llm
     global vdb
-    print("a")
 
     current_directory = os.path.dirname(os.path.realpath(__file__))
     os.chdir(current_directory)
@@ -69,15 +73,22 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@sched.scheduled_job('cron', hour='15', minute='51', id='load announcement')
+@sched.scheduled_job('cron', hour='1', minute='30', id='load announcement')
 def log_new_announcements():
     # 하루 전의 시간 계산
     one_day_ago = datetime.datetime.now(datetime.UTC) - datetime.timedelta(days=1)
     # 하루 동안 추가된 announcement 조회
     new_announcements = session.query(Announcement).filter(Announcement.writtenDate >= one_day_ago).all()
     # 조회된 announcement 로그에 기록
+    docs = []
     for announcement in new_announcements:
-        logging.info(f"New announcement: Title - {announcement.title}, Content - {announcement.url}")
+        doc = Document()
+        doc.page_content = announcement.document
+        doc.metadata['title'] = announcement.title
+        doc.metadata['datetime'] = announcement.writtenDate
+        docs.append(doc)
+    with open(f'{datetime.datetime.now(datetime.UTC)}'+'.pkl', 'wb') as f:
+            pickle.dump(docs, f)
 
 @app.get("/")
 async def initiate():
@@ -85,31 +96,23 @@ async def initiate():
     return "안녕하세요! 국민대학교 전용 챗봇 KUKU입니다. 국민대학교에 대한 건 모든 질문해주세요!"
 
 @app.post("/api/chatbot")
-async def query(query: Query, x_user_id: Optional[str] = Header(None)):
+async def query(query: Query):
     try:
-        ans = llm.query(x_user_id, query.query, query.target_lang)
+        ans = llm.query(query.query, query.target_lang)
     except:
-        return {'success': False,
-                'message': 'failed'}
-    return {'success': True,
-            'message': 'success',
-            'response': {
-                'answer': ans
-            }}
+        raise CustomException(name="chatbot error")
+    return JSONResponse(status_code=200, content={'success': True,
+                                                  'message': 'success',
+                                                  'response': {
+                                                    'answer': ans
+                                                    }})
 
-@app.get("/api/cahtbot_close")
-async def close(x_user_id: Optional[str] = Header(None)):
-    try:
-        ans = llm.query(x_user_id, query.query, query.target_lang)
-    except:
-        return {'success': False,
-                'message': 'failed'}
-    return {'success': True,
-            'message': 'success',
-            'response': {
-                'answer': ans
-            }}
 
+@app.exception_handler(CustomException)
+async def MyCustomExceptionHandler(request: Request, exception: CustomException):
+  return JSONResponse(status_code = 400,
+                      content = {'success': False,
+                                 'message': 'failed'})
 
 
 if __name__ == "__main__":
